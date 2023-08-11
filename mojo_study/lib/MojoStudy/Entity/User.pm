1cd9cd2d6ff644cc89ebb2fbab28579fec12ddf8
t1a1k1u1 <koishi.t81@gmail.com>
2023-08-11 16:29:27 +0900

    add UserGet UseCase

--- mojo_study/lib/MojoStudy/Entity/User.pm
+++ mojo_study/lib/MojoStudy/Entity/User.pm
@@ -1,6 +1,7 @@
 package MojoStudy::Entity::User;
 use Moose;
 
+has 'id' => (is => 'ro', isa => 'Int');
 has 'name' => (is => 'ro', isa => 'Str');
 
 1;
--- mojo_study/lib/MojoStudy/Repository/User.pm
+++ mojo_study/lib/MojoStudy/Repository/User.pm
@@ -1,11 +1,20 @@
 package MojoStudy::Repository::User;
-use feature 'signatures';
+use feature qw/state signatures/;
 
 use MojoStudy::Entity::User;
 
 sub create ($class, $name) {
+    state $id = 0; $id += 1;
     return MojoStudy::Entity::User->new(
+        id   => $id,
         name => $name,
+    );
+}
+
+sub get ($class, $id) {
+    return MojoStudy::Entity::User->new(
+        id   => $id,
+        name => 'dummy',
     );
 }
 
